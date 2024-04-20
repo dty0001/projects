d9176006784e93e53ab16c0f3e5435e8eb1c8e16
dty0001 <127909794+dty0001@users.noreply.github.com>
2024-04-20 12:59:42 -0400

    loadstring

--- dahood.lua
+++ dahood.lua
@@ -1,2 +1,2 @@
 script_key = getgenv().Cloud.Load.Key;
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c4936d5728327e5749fb172cfcbdc0e5.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/12b8feffa38c1d5e8bf494c8d2ea9900.lua"))()
